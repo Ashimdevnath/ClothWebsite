b480a3ff9438100cd827623c30f4602c7b331e01
Ashim Devnath <89856438+Ashimdevnath@users.noreply.github.com>
2022-09-22 21:52:48 +0545

    web side uploaded

---
+++ login.js
@@ -0,0 +1,12 @@
+function validate() {
+    let x = document.forms["myform"]["email"].value;
+    let y = document.forms["myform"]["passward"].value;
+    if (x == null || x == "") {
+        alert("Please fill the email field.");
+        return false;
+    }
+    else if (y == null || y == "") {
+        alert("Please fill the password field.");
+        return false;
+    }
+}
